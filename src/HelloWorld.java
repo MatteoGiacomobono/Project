78adcebb50ac8eff0bfc144f73038a614857dcfb
sam <sam@Sam>
2019-05-07 17:31:01 +0200

    agg.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 	public static void main ( String [] args ) {
 		// visualizza un messaggio di saluto
 		System.out.println ( " Hello World ! " );
-	    
+	    System.out.println("ciao");
 	}
 }
